f53b4795e4255b84d1e063739b2959cae0a7addf
RSKW <rafael.kwiecinski@gmail.com>
2022-11-15 22:14:18 -0300

    parser_loader.py

--- weatherterm/core/parser_loader.py
+++ weatherterm/core/parser_loader.py
@@ -3,15 +3,15 @@ import re
 import inspect
 
 
-def _get_parser_list(dirname):
-    files = [f.replace(".py", "") for f in os.listdir(dirname)
+def _get_parser_list(directory_name):
+    files = [f.replace(".py", "") for f in os.listdir(directory_name)
              if not f.startswith("__")]
     return files
 
 
-def _import_parsers(parserfiles):
+def _import_parsers(parser_files):
     m = re.compile('.+parser$', re.I)
-    _modules = __import__("weatherterm.parsers", globals(), locals(), parserfiles, 0)
+    _modules = __import__("weatherterm.parsers", globals(), locals(), parser_files, 0)
     _parsers = [(k, v) for k, v in inspect.getmembers(_modules)
                 if inspect.ismodule(v) and m.match(k)]
     _classes = dict()
@@ -20,6 +20,6 @@ def _import_parsers(parserfiles):
     return _classes
 
 
-def load(dirname):
-    parserfiles = _get_parser_list(dirname)
-    return _import_parsers(parserfiles)
+def load(directory_name):
+    parser_files = _get_parser_list(directory_name)
+    return _import_parsers(parser_files)
